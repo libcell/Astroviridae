c69b82ab0ffe75bdd418e20c8400ee511c79024f
Bo Lee <libcell@cqu.edu.cn>
2022-04-16 22:18:31 +0800

    update

---
+++ R/seq_choose.R
@@ -0,0 +1,10 @@
+
+# Some useful keyboard shortcuts for package authoring:
+
+genome_sequences <- system.file("extdata", "sample.fasta", package = "ggmsa")
+
+
+hello <- function() {
+  print("Hello, world!")
+}
+
