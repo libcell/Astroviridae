de02b1a19cf5e0e4b9369c8628cbd0d579e99f11
Bo Lee <libcell@cqu.edu.cn>
2022-04-18 00:23:19 +0800

    added

--- R/genome_check.R
+++ R/genome_check.R
@@ -1,10 +1,4 @@
 
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 genome_check <- function() {
   print("Hello, world!")
 }
