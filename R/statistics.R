a79a9cade2c1e6b8b9ffc4f90141e9a4338f6390
Bo Lee <libcell@cqu.edu.cn>
2022-04-17 13:03:56 +0800

    repaired

--- R/statistics.R
+++ R/statistics.R
@@ -1,13 +1,16 @@
 
 # Statistics information on Astroviridae.
 
-statistics <- function(seq.type = "all") {
+statistics <- function(seq.type = "All") {
   # stats <- readRDS("data/Statistics.rds")
-  if (seq.type == "all")
+  if (!(seq.type %in% c("All", "RefSeq", "NonRef")))
+    stop("Please input the right parameter seq.type, and it must be one of 'All', 'RefSeq' or 'NonRef'!")
+  if (seq.type == "All")
     content <- stats
   if (seq.type == "RefSeq")
     content <- stats[grep("NCBI RefSeq", stats$`Annotation Name`), ]
-  if (seq.type == "others")
+  if (seq.type == "NonRef")
     content <- stats[-grep("NCBI RefSeq", stats$`Annotation Name`), ]
+  rownames(content) <- 1:nrow(content)
   print(content)
 }
