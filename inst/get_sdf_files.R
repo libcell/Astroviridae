d16024d71f28eca091583d5e7ebadbef69bddfd8
Bo Lee <libcell@cqu.edu.cn>
2022-04-22 19:39:38 +0800

    added

--- inst/get_sdf_files.R
+++ inst/get_sdf_files.R
@@ -1,5 +1,5 @@
 
-## (1) install R packages. 
+## (1) install R packages.
 
 if (!require("BiocManager")) install.packages("BiocManager")
 
@@ -7,19 +7,27 @@ BiocManager::install("webchem")
 
 BiocManager::install("ChemmineR")
 
-## (2) get the CID of all compounds, taking convolamine as example. 
+## (2) get the CID of all compounds, taking convolamine as example.
 
 library(webchem)
 
 help(package = "webchem")
 
-drug <- c("convolamine", 
-          "esculetin", 
+drug <- c("convolamine",
+          "esculetin",
           "tinidazole")
+
+drug <- read.csv("CID for approved Scz drugs.txt",
+                 header = TRUE,
+                 sep = "\t")
+
+drug <- drug$Drug.name
+
+
 
 op <- par(mfrow = c(2, 2))
 
-## (3) obtain the sdf files for all compounds. 
+## (3) obtain the sdf files for all compounds.
 
 library(ChemmineR)
 
@@ -29,43 +37,44 @@ length(SCZ_drug) <- length(drug)
 
 names(SCZ_drug) <- drug
 
-SCZ_did <- paste("candidate", 
-                 1:length(drug), 
+SCZ_did <- paste("candidate",
+                 1:length(drug),
                  sep = "-")
 
-cmpd <- new("SDFset", 
-            SDF = SCZ_drug, 
+cmpd <- new("SDFset",
+            SDF = SCZ_drug,
             ID = SCZ_did)
 
 
 for (d in 1:length(drug)) {
-  cid <- get_cid(query = drug[d], 
-                 from = "name", 
+  cid <- get_cid(query = drug[d],
+                 from = "name",
                  match = "first")
   cid <- cid$cid[1]
-  
-  sdf_url <- paste("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/CID", 
-                   cid, 
-                   "record/SDF/?record_type=2d&response_type=display", 
+
+  Sys.sleep(3)
+
+  print(cid)
+
+  sdf_url <- paste("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/CID",
+                   cid,
+                   "record/SDF/?record_type=2d&response_type=display",
                    sep = "/")
-  
+
   sdfset <- read.SDFset(sdf_url)
-  
+
   plot(sdfset)
-  
+
   cmpd@SDF[d] <- sdfset@SDF
-  
-  cmpd@ID[d] <- sdfset@ID
-  
+
+  cmpd@ID[d] <- drug[d]
+
+  # cid(cmpd) <- makeUnique(cid(cmpd))
+
 }
 
 par(op)
 
+plot(cmpd)
 
-
-
-
-sdfset <- read.SDFset("http://faculty.ucr.edu/~tgirke/Documents/R_BioCond/Samples/sdfsample.sdf") 
-
-plot(sdfset[1:4])
 
